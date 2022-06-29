9f2423a60237c817ba193e56d23bcdb19c17f5a1
frsargua <frsargua@gmail.com>
2022-06-29 23:20:35 +0100

    added routes to my server

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
-const db = require("./db/db.json");
+// const db = require("./db/db.json");
+const routes = require("./routes/index");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,42 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Loads notes.html file
-app.get("/notes", (req, res) => {
-  // res.sendFile();
-  res.sendFile(
-    "/Users/fabiansarango/Desktop/Bootcamp-2022/Bootcamp Weekly HW/week11-HW/Develop/public/notes.html"
-  );
-});
-
-// Loads database file
-app.get("/api/notes", (req, res) => res.json(db));
-
-//Deletes notes based on their id
-app.delete("/api/notes/:id", (req, res) => {
-  const idParam = parseInt(req.params.id);
-  const isIdInDb = db.some((value) => value.id == idParam);
-  if (isIdInDb) {
-    res.send({
-      message: "deleted",
-      changes: `Note with Id: ${idParam} has been deleted `,
-      object: db.pop(),
-    });
-  }
-  res.status(404).json({ id: "Id does not exists!" });
-  return;
-});
-
-//Creates a new note from object passed by the client
-app.post("/api/notes", (req, res) => {
-  const newNoteObject = req.body;
-  db.push(newNoteObject);
-  res.send({
-    message: "Created",
-    changes: `Note with Id: ${newNoteObject.id} has been created `,
-    object: newNoteObject,
-  });
-});
+app.use(routes);
 
 // Loads index.html file if url is anything other than notes.
 app.get("*", (req, res) =>
